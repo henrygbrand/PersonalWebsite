687ca2b2dca6653e5da224f7e2c3c283b9116a52
Henry <hbrand@vols.utk.edu>
2023-02-02 00:48:39 -0500

    m

--- rainbow.js
+++ rainbow.js
@@ -3,5 +3,7 @@ window.addEventListener('load', function() {
     element.onmouseover = function() {
         element.style.color = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
         element.style.fontSize = `${Math.floor(Math.random() * 100) + 2}px`;
+        element.style.fontFamily = "VT323";
+        element.style.marginLeft = `${Math.floor(Math.random() * 200)}`;
     }
 });
