e1ef1d2db716b39bc44e5ed6e4ddfe833b3cd30e
Henry <hbrand@vols.utk.edu>
2023-02-02 00:43:55 -0500

    done with this for now

---
+++ rainbow.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', function() { 
+    let element = document.getElementById('text');
+    element.onmouseover = function() {
+        element.style.color = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
+        element.style.fontSize = `${Math.floor(Math.random() * 100) + 2}px`;
+    }
+});
